44a04d17668ab6f16ebdf81361d82b4c50598dae
dyorgens <35741856+dyorgens@users.noreply.github.com>
2018-03-29 17:06:13 -0600

    Updated .h file
    I put the .cpp in this file by accident.

--- uMatrix.h
+++ uMatrix.h
@@ -6,112 +6,82 @@
 //Create a program which will perform the matrix addition and multiplication of 
 //2 matricies as well as the scalar addition and multiplication of those 
 //matricies
-
-#include <iostream>
-#include "MatrixClass.h"
-using namespace std;
-
-uMatrix::uMatrix(int matrix[100][100], int row, int col)
+class uMatrix
 {
-	rows = row;
-	cols = col;
-	for (int i = 0; i < 100; ++i)
-		for (int j = 0; j < 100; ++j)
-		{
-			myMatrix[i][j] = matrix[i][j];
-		}
-}
-uMatrix::~uMatrix()
-{
-}
+private:
+	
 
-void uMatrix::print()
-{
+public:
+	int myMatrix[100][100];
+	int rows, cols;
+	//You probably should have some privately accessible helper things ...
+	// like data structures?
 
-	for (int i = 0; i < rows; ++i)
-		for (int j = 0; j < cols; ++j)
-		{
-			cout << " " << myMatrix[i][j];
-			if (j == cols - 1)
-				cout << endl;
-		}
-}
+	uMatrix(int matrix[100][100], int row, int col);
+	~uMatrix();
 
-uMatrix uMatrix::MM(uMatrix B,int r1, int c1, int r2, int c2, int i, int j, int k)
-{
-	int mult[100][100] = { 0 };
+	/**
+	* @brief print
+	* @input one object, self, of type uMatrix A,
+	* @output a square representation of the matrix A
+	*/
+	void print();
 
-	// Initializing elements of matrix mult to 0.
-	for (i = 0; i < r1; ++i)
-		for (j = 0; j < c2; ++j)
-		{
-			mult[i][j] = 0;
-		}
-	//Matrix multiplication of the 2 matricies
-    for (i = 0; i < r1; ++i)
-		for (j = 0; j < c2; ++j)
-			for (k = 0; k < c1; ++k)
-			{
-				mult[i][j] += myMatrix[i][k] * B.myMatrix[k][j];
-			}
+	/**
+	* @brief MM Matrix Multiplication
+	* @input two objects of type uMatrix self A, and parameter B
+	* @output a new object of type uMatrix = A x B what is matrix multiplication?
+	* https://en.wikipedia.org/wiki/Matrix_multiplication
+	*/
+	uMatrix MM(uMatrix B, int r1, int c1, int r2, int c2, int i, int j, int k);
 
-	return uMatrix(mult, rows, cols);
-}
+	/**
+	* @brief MMf Fast Matrix Multiplication
+	* @input two objects of type uMatrix self A, and parameter B
+	* @output a new object of type uMatrix = A x B what is matrix multiplication?
+	* https://en.wikipedia.org/wiki/Matrix_multiplication
+	*/
+	//uMatrix MMf(uMatrix B);
 
-//uMatrix uMatrix::MMf(uMatrix B)
-//{
-//}
+	/**
+	* @brief MA Matrix Addition
+	* @input two objects of type uMatrix self A, and parameter B
+	* @output a new object of type uMatrix = A + B
+	*/
+	uMatrix MA(uMatrix B, int r1, int c1, int r2, int c2, int i, int j, int k);
+	/**
+	* @brief MA Fast Matrix Addition
+	* @input two objects of type uMatrix, self A, and parameter B
+	* @output a new object of type uMatrix = A + B
+	*/
+	//uMatrix MAf(uMatrix B);
 
-uMatrix uMatrix::MA(uMatrix B, int r1, int c1, int r2, int c2, int i, int j, int k)
-{
-	int sum[100][100] = { 0 };
+	/**
+	* @brief SM Scalar Multiplication
+	* @input one object, self, of type uMatrix A, and a scalar value S
+	* @output a new object of type uMatrix such that every element in A is multiplied by S
+	*/
+	uMatrix SM(uMatrix B, int sc, int r1, int c1, int r2, int c2, int i, int j, int k);
 
-	// Initializing elements of matrix sum to 0.
-	for (i = 0; i < r1; ++i)
-		for (j = 0; j < c2; ++j)
-		{
-			sum[i][j] = 0;
-		}
-	//Matrix Addition of the 2 matricies
-	for (i = 0; i < r1; ++i)
-		for (j = 0; j < c2; ++j)
-			sum[i][j] = myMatrix[i][j] + B.myMatrix[i][j];
-	return uMatrix(sum, rows, cols);
-}
+	/**
+	* @brief SMf Fast Scalar Multiplication
+	* @input one object, self, of type uMatrix A, and a scalar value S
+	* @output a new object of type uMatrix such that every element in A is multiplied by S
+	*/
+	//uMatrix SMf(int S);
 
-//uMatrix uMatrix::MAf(uMatrix B)
-//{
-//}
+	/**
+	* @brief SA Scalar Addition
+	* @input one object, self, of type uMatrix A, and a scalar value S
+	* @output a new object of type uMatrix such that every element in A is increased by S
+	*/
+	uMatrix SA(uMatrix B, int sc, int r1, int c1, int r2, int c2, int i, int j, int k);
+	/**
+	* @brief SAf Fast Scalar Addition
+	* @input one object, self, of type uMatrix A, and a scalar value S
+	* @output a new object of type uMatrix such that every element in A is increased by S
+	*/
+	//uMatrix SAf(int S);
 
-uMatrix uMatrix::SM(uMatrix B, int sc, int r1, int c1, int r2, int c2, int i, int j, int k)
-{
-	int scalar[100][100] = { 0 };
-	//Scalar multiplication of the 2 matricies
-	for (j = 0; j < r1; ++j)
-		for (k = 0; k < c1; ++k)
-		{
-			scalar[j][k] = sc * myMatrix[j][k];
-		}
-	return uMatrix(scalar, rows, cols);
-}
 
-//uMatrix uMatrix::SMf(int S)
-//{
-//}
-
-uMatrix uMatrix::SA(uMatrix B, int sc, int r1, int c1, int r2, int c2, int i, int j, int k)
-{
-		int scalar[100][100] = { 0 };
-				//Scalar multiplication of the 2 matricies
-					for (j = 0; j < r1; ++j)
-						for (k = 0; k < c1; ++k)
-						{
-							scalar[j][k] = sc + myMatrix[j][k];
-							
-						}
-							return uMatrix(scalar, rows, cols);
-}
-
-//uMatrix uMatrix::SAf(int S)
-//{
-//}
+};
